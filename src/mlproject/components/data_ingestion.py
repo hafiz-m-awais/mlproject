8593a2a31e3ba40fdca3bf863f2745fe80c32702
Hafiz Muhammad Awais <hafiz.awais.ds@gmail.com>
2026-03-30 15:52:21 +0500

    EDA and Models training

--- src/mlproject/components/data_ingestion.py
+++ src/mlproject/components/data_ingestion.py
@@ -16,7 +16,7 @@ from dataclasses import dataclass
 class DataIngestionConfig:
     train_data_path:str=os.path.join('artifacts','train.csv')
     test_data_path:str=os.path.join('artifacts','test.csv')
-    raw_data_path:str=os.path.join('artifacts','data.csv')
+    raw_data_path:str=os.path.join('artifacts','raw.csv')
 
 class DataIngestion:
     def __init__(self):
