28c1d05fa202cc46709cb9c5319b80349e0ac8ee
Hafiz Muhammad Awais <hafiz.awais.ds@gmail.com>
2026-03-31 12:08:51 +0500

    Model training

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import sys
 from src.mlproject.components.data_ingestion import DataIngestion
 from src.mlproject.components.data_ingestion import DataIngestionConfig
 from src.mlproject.components.data_transformation import DataTransformationConfig,DataTransformation
+from src.mlproject.components.model_trainer import ModelTrainerConfig,ModelTrainer
 
 if __name__=="__main__":
     logging.info("The execution has started")
@@ -19,10 +20,15 @@ if __name__=="__main__":
 
         #data_transformation_config=DataTransformationConfig
         data_transformation=DataTransformation()
-        data_transformation.initiate_data_transformation(
+        train_array, test_array, _ = data_transformation.initiate_data_transformation(
             train_path=train_data_path,
             test_path=test_data_path
         )
+
+        ##Model Trainer
+        model_trainer=ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_array,test_array))
+
     except Exception as e:
         logging.info("custom exception has occurred")
         raise CustomException(e,sys)
